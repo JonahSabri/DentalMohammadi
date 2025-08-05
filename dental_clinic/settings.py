8cfbe1da1a2ade2ed7a463fc0e652d0375441813
GOD YNS <yon@hi2.in>
2025-08-05 07:18:30 -0700

    All

--- dental_clinic/settings.py
+++ dental_clinic/settings.py
@@ -108,7 +108,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://dentalmohammadi.ir:3000",
     "http://dentalmohammadi.ir",
     "http://dentalmohammadi.ir:8000",
+    "http://dentalmohammadi.ir:80",
+    "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "http://127.0.0.1:80",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
